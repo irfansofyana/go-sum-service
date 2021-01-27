641b1df5e2136704c64fc2d28a0be94cfa8cd0b4
irfansofyana <irfansofyana0305@gmail.com>
2021-01-27 07:40:53 +0700

    Refactor code

--- main.go
+++ main.go
@@ -2,42 +2,43 @@ package main
 
 import (
 	"fmt"
-	"sum/utils"
 	"log"
 	"net/http"
+	
+	"sum/utils"
 	m"sum/middleware"
+
+	"github.com/urfave/negroni"
+	"github.com/gorilla/mux"
 )
 
-func GetSum(w http.ResponseWriter, r *http.Request) {
+func GetSumHandler(w http.ResponseWriter, r *http.Request) {
 	var a, _ = r.URL.Query()["a"]
 	var b, _ = r.URL.Query()["b"]
 
 	result, err := utils.Sum(a[0], b[0])
 	
-	w.Header().Set("Content-Type", "application/json")
-
 	if (err != nil) {
 		message := "Internal server error"
-		m.GenerateJSONResponse(w, r, 500, message, "")
+		utils.GenerateJSONResponse(w, r, 500, message, "")
 		return
 	}
 
-	message := "Sum function is success"
-	m.GenerateJSONResponse(w, r, 200, message, result)
+	message := "Success"
+	utils.GenerateJSONResponse(w, r, 200, message, result)
 }
 
 func main() {
-	mux := http.DefaultServeMux
-
-	mux.HandleFunc("/sum", GetSum)
-	
-	var handler http.Handler = mux
-	handler = m.SumMiddleware(handler)
-	
-	server := new(http.Server)
-	server.Addr = ":8080"
-	server.Handler = handler
-
 	fmt.Println("Sum service started at localhost:8080")
-	log.Fatal(server.ListenAndServe())
+
+	router := mux.NewRouter()
+	router.Methods("GET").
+		Path("/sum").
+		HandlerFunc(GetSumHandler)
+
+	n := negroni.New()
+	n.Use(negroni.HandlerFunc(m.GetSumHandler))
+	n.UseHandler(router)
+	
+	log.Fatal(http.ListenAndServe(":8080", n))
 }
--- middleware/sum_middleware.go
+++ middleware/sum_middleware.go
@@ -5,29 +5,60 @@ import (
 	"sum/utils"
 )
 
-func SumMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		var a, isExistA = r.URL.Query()["a"]
-		var b, isExistB = r.URL.Query()["b"]
+// func SumMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+// 		var a, isExistA = r.URL.Query()["a"]
+// 		var b, isExistB = r.URL.Query()["b"]
 
-		if (!isExistA || !isExistB) {	
-			message := "Variable a and/or b is not available on query parameter"
-			GenerateJSONResponse(w, r, 400, message, "")
-			return
-		}
+// 		if (!isExistA || !isExistB) {	
+// 			message := "Variable a and/or b is not available on query parameter"
+// 			utils.GenerateJSONResponse(w, r, 400, message, "")
+// 			return
+// 		}
 
-		if (len(a) != 1 || len(b) != 1) {
-			message := "Ambigous value on variable a and/or b"
-			GenerateJSONResponse(w, r, 400, message, "")
-			return
-		}
+// 		if (len(a) != 1 || len(b) != 1) {
+// 			message := "Ambigous value on variable a and/or b"
+// 			utils.GenerateJSONResponse(w, r, 400, message, "")
+// 			return
+// 		}
 
-		if (!utils.IsInt(a[0]) || !utils.IsInt(b[0])) {
-			message := "Variable a and/or is not a valid integer"
-			GenerateJSONResponse(w, r, 400, message, "")
-			return
-		}
+// 		if (!utils.IsInt(a[0]) || !utils.IsInt(b[0])) {
+// 			message := "Variable a and/or is not a valid integer"
+// 			utils.GenerateJSONResponse(w, r, 400, message, "")
+// 			return
+// 		}
 
-		next.ServeHTTP(w, r)
-	})
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
+
+func GetSumHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	var a, isExistA = r.URL.Query()["a"]
+	var b, isExistB = r.URL.Query()["b"]
+
+	if (!isExistA || !isExistB) {	
+		message := "Variable a and/or b is not available on query parameter"
+		utils.GenerateJSONResponse(w, r, 400, message, "")
+		return
+	}
+
+	if (len(a) != 1 || len(b) != 1) {
+		message := "Ambigous value on variable a and/or b"
+		utils.GenerateJSONResponse(w, r, 400, message, "")
+		return
+	}
+
+	if (!utils.IsInt(a[0]) || !utils.IsInt(b[0])) {
+		message := "Variable a and/or is not a valid integer"
+		utils.GenerateJSONResponse(w, r, 400, message, "")
+		return
+	}
+
+	next(w, r)
+}
+
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// do some stuff before
+	next(rw, r)
+	// do some stuff after
 }
